44f7077b9ff3d1f66cf889c8fdbf09af552a09f9
Daniel Tsai <b09303052@ntu.edu.tw>
2023-06-13 11:37:39 +0800

    哭哭

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -82,27 +82,29 @@ for row, item in publications.iterrows():
     md += "\ndate: " + str(item.pub_date) 
     
     md += "\nvenue: '" + html_escape(item.venue) + "'"
-    """ 
+    
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
     md += "\ncitation: '" + html_escape(item.citation) + "'"
     
     md += "\n---"
-    """
+    
     ## Markdown description for individual page
-    """
+    
     if len(str(item.paper_url)) > 5:
         md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
     if len(str(item.excerpt)) > 5:
-        md += "\nRecommended citation:" + html_escape(item.excerpt) + "\n"
+        md += "\n" + html_escape(item.excerpt) + "\n"
         
     md += item.citation
     
     md_filename = os.path.basename(md_filename)
        
     with open("../_publications/" + md_filename, 'w') as f:
-        f.write(md)"""
+        f.write(md)
 
 
+
+# %%
